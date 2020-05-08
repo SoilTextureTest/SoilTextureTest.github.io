57e66057ba160569caa992e55d0103f26f974e3c
SoilTextureTest <64961262+SoilTextureTest@users.noreply.github.com>
2020-05-08 18:28:26 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,173 @@
+questions = [
+    {
+        "question": "It feels dry, or even warm.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It has a yellowish color.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "When touching it, it feels quite sticky.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The material feels kind of smooth.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A crunchy sound is made when stepping on the material.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The material feels kind of humid, even when dry.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 5,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Tiny powder or particles fall down when I pick it up.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 5,
+            "scty": -5
+        }
+    },
+    {
+        "question": "It is hard to crunch up the material.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "If I drop it on the ground from about 30cm or 10 Inches, it spreads out like powder or liquid.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 5,
+            "scty": -5
+        }
+    },
+    {
+        "question": "The material can go through the gap between my fingers.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 5,
+            "scty": -5
+        }
+    },
+    {
+        "question": "The material is a solid.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Putting it in a cup of water makes it dissolve.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 5,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Adding water to it makes it change color.",
+        "effect": {
+            "econ": 5,
+            "dipl": 5,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "The material looks like or is a building material.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "The material is dust-like.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The material originates from rocks.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The material has a volcanic origin.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "If a use a hammer to hit on it, it will crack.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "The texture is kind of pointy, as it feels a bit sharp.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    }
+];
