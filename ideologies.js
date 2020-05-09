29797ee9aee310fbb189437e8da2b0ddb2ba60c7
SoilTextureTest <64961262+SoilTextureTest@users.noreply.github.com>
2020-05-09 14:14:39 +0800

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,23 @@
+ideologies = [
+    
+    {
+        "name": "Test",
+        "desc": "Test message",
+        "stats": {
+            "econ": 10,
+            "dipl": 85,
+            "govt": 90,
+            "scty": 10,
+        }
+    },
+    {
+        "name": "Test",
+        "desc": "Test message",
+        "stats": {
+            "econ": 20,
+            "dipl": 35,
+            "govt": 90,
+            "scty": 50,
+        }
+    }
+    ];
