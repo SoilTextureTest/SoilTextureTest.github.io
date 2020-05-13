b2608aa80310f6c2f54f2e5defe94a7c06d05d9b
SoilTextureTest <64961262+SoilTextureTest@users.noreply.github.com>
2020-05-13 17:55:46 +0800

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,23 +1,121 @@
 ideologies = [
-    
+//Powder
     {
-        "name": "Test",
-        "desc": "Test message",
+        "name": "Clay",
         "stats": {
-            "econ": 10,
-            "dipl": 85,
-            "govt": 90,
-            "scty": 10,
+            "econ": 0,
+            "dipl": 100,
+            "govt": 50,
+            "scty": 50
         }
     },
     {
-        "name": "Test",
-        "desc": "Test message",
+        "name": "Sand",
+        "stats": {
+            "econ": 100,
+            "dipl": 20,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Loamy Sand",
+        "stats": {
+            "econ": 80,
+            "dipl": 10,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Sandy Loam",
+        "stats": {
+            "econ": 60,
+            "dipl": 15,
+            "govt": 60,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Sandy Clay Loam",
+        "stats": {
+            "econ": 70,
+            "dipl": 30,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Sandy Clay",
+        "stats": {
+            "econ": 70,
+            "dipl": 50,
+            "govt": 40,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Loam",
+        "stats": {
+            "econ": 50,
+            "dipl": 30,
+            "govt": 65,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Clay Loam (Centrist)",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Silty Clay",
         "stats": {
             "econ": 20,
-            "dipl": 35,
-            "govt": 90,
-            "scty": 50,
+            "dipl": 65,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Silty Clay Loam",
+        "stats": {
+            "econ": 15,
+            "dipl": 50,
+            "govt": 65,
+            "scty": 50
+        }
+    },
+     {
+        "name": "Silt Loam",
+        "stats": {
+            "econ": 30,
+            "dipl": 25,
+            "govt": 80,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Silt",
+        "stats": {
+            "econ": 0,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    //Void
+    {
+        "name": "Void",
+        "stats": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
         }
     }
-    ];
+];
