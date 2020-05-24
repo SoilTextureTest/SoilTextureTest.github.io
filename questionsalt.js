66490316f56d9d3f00bc09ddee211a103d3095da
SoilTextureTest <64961262+SoilTextureTest@users.noreply.github.com>
2020-05-24 17:56:56 +0800

    Create questionsalt.js

---
+++ questionsalt.js
@@ -0,0 +1,110 @@
+questions = [
+    {
+        "question": "I support societal development.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Ancient Egyptian infrastructure looks the best within the Ancient World.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Agriculture is more important then business development.",
+        "effect": {
+            "econ": 2,
+            "dipl": 2,
+            "govt": 0,
+            "scty": -3
+        }
+    },
+    {
+        "question": "I go with the flow and follow what other people do.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "I support rapid industrialization.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Strength is more important than persuasion.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 8
+        }
+    },
+    {
+        "question": "The ultimate form of power comes from strength and combat.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Clay and Concrete are vital materials that led to modern infrastrucuture.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Due to the fact that Sand can potentially hurt people's eyes with wind, they should be kept away from children.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Walls around borders are important for self-defence and reducing criminality.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Sandy Deserts are better than Mesa Deserts.",
+        "effect": {
+            "econ": 5,
+            "dipl": -5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The texture is kind of pointy, as it feels a bit sharp.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    }
+];
